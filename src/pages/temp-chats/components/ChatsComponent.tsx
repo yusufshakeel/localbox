a682253b67de9fa9b4eea465751a66ef15cfebce
Yusuf Shakeel <6522504+yusufshakeel@users.noreply.github.com>
2024-12-10 21:19:12 +0530

    update

--- src/pages/temp-chats/components/ChatsComponent.tsx
+++ src/pages/temp-chats/components/ChatsComponent.tsx
@@ -19,17 +19,9 @@ export default function ChatsComponent(props: PropType) {
   }, [props.messages]);
 
   return (
-    <div
-      ref={messagesEndRef}
-      style={{
-        padding: '10px',
-        height: '300px',
-        overflowY: 'scroll',
-        borderRadius: '0'
-      }}
-    >
+    <div ref={messagesEndRef} style={{ padding: '10px', height: '300px', overflowY: 'scroll', borderRadius: '0'}}>
       {
-        props.messages.map((msg: MessageType, idx) => (
+        (props.messages || []).map((msg: MessageType, idx) => (
           <div key={idx}
             style={{ textAlign: msg.userId === props.currentUserId ? 'right' : 'left' }}>
             <div className={`${msg.userId === props.currentUserId ? 'bg-light': ''} card mb-3 p-2`} style={{display: 'inline-block'}}>
