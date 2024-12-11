9df76d538c70db8dd9d06799ee46789b47263454
Yusuf Shakeel <6522504+yusufshakeel@users.noreply.github.com>
2024-12-12 00:05:50 +0530

    support for audio, image, video in temp-chats

--- src/components/temp-chats/ChatsComponent.tsx
+++ src/components/temp-chats/ChatsComponent.tsx
@@ -25,12 +25,41 @@ export default function ChatsComponent(props: PropType) {
     <div ref={messagesEndRef} style={{ padding: '10px', height: '300px', overflowY: 'scroll', borderRadius: '0'}}>
       {
         (props.messages || []).map((msg: MessageType, idx) => {
+          const fileName = getFilename(msg.message);
           const message =
             msg.type === 'file'
-              ? <a className="ys-a-link" href={`/temp-chats/${encodeURIComponent(msg.message)}`} download>
-                {getFilename(msg.message)} <FontAwesomeIcon icon={faDownload}/>
-              </a>
+              ? <div>
+                <a className="ys-a-link" href={`/temp-chats/${encodeURIComponent(msg.message)}`} download>
+                  {fileName} <FontAwesomeIcon icon={faDownload}/>
+                </a>
+              </div>
               : <span>{msg.message}</span>;
+
+          let messagePreview: any = ''
+          if (msg.type === 'file') {
+            if (['jpg', 'jpeg', 'png'].includes(fileName.split('.')?.pop() ?? '')) {
+              messagePreview = (
+                <img src={`/temp-chats/${encodeURIComponent(msg.message)}`} className="img-fluid"
+                  style={{maxHeight: '200px'}}/>
+              );
+            }
+            else if (['mp4'].includes(fileName.split('.')?.pop() ?? '')) {
+              messagePreview = (
+                <video controls height="120px" key={msg.message}>
+                  <source src={`/temp-chats/${encodeURIComponent(msg.message)}`}/>
+                  Your browser does not support the video tag.
+                </video>
+              );
+            }
+            else if (['mp3'].includes(fileName.split('.')?.pop() ?? '')) {
+              messagePreview = (
+                <audio controls key={msg.message}>
+                  <source src={`/temp-chats/${encodeURIComponent(msg.message)}`}/>
+                  Your browser does not support the audio tag.
+                </audio>
+              );
+            }
+          }
           return (
             <div key={idx}
               style={{textAlign: msg.userId === props.currentUserId ? 'right' : 'left'}}>
@@ -43,6 +72,7 @@ export default function ChatsComponent(props: PropType) {
                     className="float-end ms-3"> {new Date(msg.timestamp).toLocaleString()}</span>
                 </div>
                 <div className="mb-1" style={{wordBreak: 'break-word', overflowWrap: 'break-word'}}>
+                  {messagePreview}
                   {message}
                 </div>
               </div>
