c1a72af713a8636f6cf079b789ad4889d33f3aa2
Yusuf Shakeel <6522504+yusufshakeel@users.noreply.github.com>
2024-12-15 13:50:01 +0530

    Refactoring, test

--- src/effects/useInfoEffect.ts
+++ src/effects/useInfoEffect.ts
@@ -1,14 +1,16 @@
 import {useEffect, useState} from 'react';
-import {InfoType} from '@/pages/api/info';
+import httpClient from '@/api-clients';
+import {InfoType} from '@/types/info-type';
 
 const useInfoEffect = () => {
   const [info, setInfo] = useState<InfoType>();
 
   useEffect(() => {
     const fetchIp = async () => {
-      const response = await fetch('/api/info');
-      const data = await response.json();
-      setInfo(data);
+      const response = await httpClient.get<InfoType>('/api/info');
+      if (response.statusCode === 200) {
+        setInfo(response.data);
+      }
     };
     fetchIp();
   }, []);
