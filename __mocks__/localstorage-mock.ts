8500776d46edcd7230e336a396d662297e713754
Yusuf Shakeel <6522504+yusufshakeel@users.noreply.github.com>
2024-12-15 14:36:29 +0530

    Add tests for useUserPreferenceEffect

---
+++ __mocks__/localstorage-mock.ts
@@ -0,0 +1,15 @@
+export const localStorageMock = (() => {
+  let store: Record<string, string> = {};
+  return {
+    getItem: (key: string) => store[key] || null,
+    setItem: (key: string, value: string) => {
+      store[key] = value;
+    },
+    removeItem: (key: string) => {
+      delete store[key];
+    },
+    clear: () => {
+      store = {};
+    }
+  };
+})();
