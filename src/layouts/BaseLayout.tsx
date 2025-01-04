efcae9f16235bf6d50138b9a117e73070c293a3a
Yusuf Shakeel <6522504+yusufshakeel@users.noreply.github.com>
2025-01-04 11:22:02 +0530

    UI fix

--- src/layouts/BaseLayout.tsx
+++ src/layouts/BaseLayout.tsx
@@ -31,13 +31,13 @@ export default function BaseLayout(
             <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
               <div className="flex items-center gap-2">
                 <SidebarTrigger className="-ml-1"/>
-                <Separator orientation="vertical" className="mr-2 h-4"/>
+                <Separator orientation="vertical" className="h-4"/>
                 <Link href="/">
                   <Button variant="ghost">
                     <Home/>
                   </Button>
                 </Link>
-                <Separator orientation="vertical" className="mr-2 h-4"/>
+                <Separator orientation="vertical" className="h-4"/>
                 <Breadcrumb>
                   <BreadcrumbList>
                     <BreadcrumbItem>
