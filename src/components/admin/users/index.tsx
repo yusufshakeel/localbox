0f9a9e35843b9ff10d6b4abdf8012de520331598
Yusuf Shakeel <6522504+yusufshakeel@users.noreply.github.com>
2025-01-07 00:39:21 +0530

    Update user

--- src/components/admin/users/index.tsx
+++ src/components/admin/users/index.tsx
@@ -1,12 +1,26 @@
 import ListUsers from '@/components/admin/users/ListUsers';
 import CreateUser from '@/components/admin/users/CreateUser';
+import {useState} from 'react';
+import UpdateUser from '@/components/admin/users/UpdateUser';
 
 export default function UserManagementComponent() {
+  const [lastUserAccountChangesAt, setLastUserAccountChangesAt] = useState('');
+  const [userAccountToUpdate, setUserAccountToUpdate] = useState<any>(null);
+
   return (
     <div className="mt-5">
-      <CreateUser />
+      <CreateUser setLastUserAccountChangesAt={setLastUserAccountChangesAt}/>
+      <UpdateUser
+        userAccountToUpdate={userAccountToUpdate}
+        setUserAccountToUpdate={setUserAccountToUpdate}
+        setLastUserAccountChangesAt={setLastUserAccountChangesAt}
+      />
       <div className="my-5">
-        <ListUsers/>
+        <ListUsers
+          lastUserAccountChangesAt={lastUserAccountChangesAt}
+          setLastUserAccountChangesAt={setLastUserAccountChangesAt}
+          setUserAccountToUpdate={setUserAccountToUpdate}
+        />
       </div>
     </div>
   );
--- src/pages/api/admins/users.tsx
+++ src/pages/api/admins/users.tsx
@@ -3,58 +3,122 @@ import {hasAdminApiPrivileges} from '@/services/api-service';
 import {HttpMethod} from '@/types/api';
 import {UserStatus, UserType} from '@/types/users';
 import {db, UsersCollectionName} from '@/configs/database/users';
-import {userCreateSchema} from '@/validations/user-validation';
+import {userCreateSchema, userUpdateSchema} from '@/validations/user-validation';
 import {getISOStringDate} from '@/utils/date';
+import passwordService from '@/services/password-service';
 
 async function getHandler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const where = { type: UserType.user };
-  const attributes = ['id', 'username', 'displayName', 'type', 'permissions', 'createdAt', 'updatedAt'];
-  const users = await db.query.selectAsync(UsersCollectionName, { where, attributes });
-  return res.status(200).json({users});
+  try {
+    const where = {type: UserType.user};
+    const attributes = ['id', 'username', 'displayName', 'type', 'permissions', 'createdAt', 'updatedAt'];
+    const users = await db.query.selectAsync(UsersCollectionName, {where, attributes});
+    return res.status(200).json({users});
+  } catch (error: any) {
+    return res.status(500).json({message: error.message});
+  }
 }
 
 async function postHandler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const parsedData = await userCreateSchema.safeParseAsync(req.body);
-  if (!parsedData.success) {
-    return res.status(401).json({ error: parsedData});
+  try {
+    const parsedData = await userCreateSchema.safeParseAsync(req.body);
+    if (!parsedData.success) {
+      return res.status(401).json({ error: parsedData});
+    }
+
+    const dataToInsert = {
+      ...parsedData.data,
+      password: passwordService.hashPassword(parsedData.data.password),
+      permissions: [],
+      type: UserType.user,
+      status: UserStatus.active,
+      createdAt: getISOStringDate()
+    };
+
+    const userExists = await db.query.selectAsync(
+      UsersCollectionName,
+      { where: { username: parsedData.data.username } }
+    );
+    if (userExists.length) {
+      return res.status(400).json({message: 'Username already exists'});
+    }
+
+    const id = await db.query.insertAsync(UsersCollectionName, dataToInsert);
+    return res.status(201).json({id});
+  } catch (error: any) {
+    return res.status(500).json({message: error.message});
   }
-  const dataToInsert = {
-    ...parsedData.data,
-    permissions: [],
-    type: UserType.user,
-    status: UserStatus.active,
-    createdAt: getISOStringDate()
-  };
-  const id = await db.query.insertAsync(UsersCollectionName, dataToInsert);
-  return res.status(201).json({id});
+}
+
+async function patchHandler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    if (!req.query.userId) {
+      return res.status(401).json({ error: 'User Id is missing' });
+    }
+    const where = { id: req.query.userId };
+
+    const userExists = await db.query.selectAsync(UsersCollectionName, { where });
+    if (userExists.length !== 1) {
+      return res.status(400).json({message: 'User not found'});
+    }
+    const user = userExists[0];
+
+    const parsedData = await userUpdateSchema.safeParseAsync(req.body);
+    if (!parsedData.success) {
+      return res.status(401).json({ error: parsedData});
+    }
+
+    const dataToUpdate = {
+      ...user,
+      ...parsedData.data,
+      updatedAt: getISOStringDate()
+    };
+    if (parsedData.data.password) {
+      dataToUpdate['password'] = passwordService.hashPassword(parsedData.data.password);
+    }
+
+    const updatedRows = await db.query.updateAsync(UsersCollectionName, dataToUpdate, {where});
+    if (updatedRows) {
+      return res.status(200).json({id: req.query.userId});
+    }
+    return res.status(401).json({ error: 'Failed to update user' });
+  } catch (error: any) {
+    return res.status(500).json({message: error.message});
+  }
 }
 
 async function deleteHandler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (!req.query.userId) {
-    return res.status(401).json({ error: 'User Id is missing' });
+  try {
+    if (!req.query.userId) {
+      return res.status(401).json({error: 'User Id is missing'});
+    }
+    const where = {type: UserType.user, id: req.query.userId};
+    const deletedCount = await db.query.deleteAsync(UsersCollectionName, {where});
+    if (deletedCount) {
+      return res.status(200).json({id: req.query.userId});
+    }
+    return res.status(401).json({error: 'Failed to delete user'});
+  } catch (error: any) {
+    return res.status(500).json({message: error.message});
   }
-  const where = { type: UserType.user, id: req.query.userId };
-  const deletedCount = await db.query.deleteAsync(UsersCollectionName, { where });
-  if (deletedCount) {
-    return res.status(200).json({id: req.query.userId});
-  }
-  return res.status(401).json({ error: 'Failed to delete user' });
 }
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const allowedMethods = [HttpMethod.GET, HttpMethod.POST, HttpMethod.DELETE];
+  const allowedMethods = [HttpMethod.GET, HttpMethod.POST, HttpMethod.DELETE, HttpMethod.PATCH];
   if (!await hasAdminApiPrivileges(req, res, { allowedMethods })) {
     return;
   }
@@ -65,6 +129,10 @@ export default async function handler(
 
   if (req.method === HttpMethod.POST) {
     return await postHandler(req, res);
+  }
+
+  if (req.method === HttpMethod.PATCH) {
+    return await patchHandler(req, res);
   }
 
   if (req.method === HttpMethod.DELETE) {
