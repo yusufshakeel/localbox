b3cb850928e57d74219e54002ac3335cdc6582b8
Yusuf Shakeel <6522504+yusufshakeel@users.noreply.github.com>
2025-01-07 16:12:57 +0530

    Refactoring temp chats

--- src/configs/temp-chats.ts
+++ src/configs/temp-chats.ts
@@ -2,15 +2,3 @@
 
 // Set the TTL (in milliseconds) for the messages
 export const TEMP_CHATS_MESSAGE_TTL_IN_MILLISECONDS = 30 * 60 * 1000;
-
-// Set max character limit for the display name of the user
-export const TEMP_CHATS_MESSAGES_USER_DISPLAY_NAME_MAX_LENGTH = 20;
-
-// ================ DO NOT CHANGE THE FOLLOWING ====================
-
-// Set user id and display name
-export const TEMP_CHATS_MESSAGES_USER_ID = 'TEMP_CHATS_MESSAGES_USER_ID';
-export const TEMP_CHATS_MESSAGES_USER_DISPLAY_NAME = 'TEMP_CHATS_MESSAGES_USER_DISPLAY_NAME';
-
-// This will be a boolean value that will be set by the application
-export const TEMP_CHATS_MESSAGES_USER_LOGGED_IN = 'TEMP_CHATS_MESSAGES_USER_LOGGED_IN';
--- src/pages/temp-chats.tsx
+++ src/pages/temp-chats.tsx
@@ -2,77 +2,40 @@ import BaseLayout from '@/layouts/BaseLayout';
 import {TEMP_CHATS_MESSAGE_TTL_IN_MILLISECONDS} from '@/configs/temp-chats';
 import useChattingEffect from '@/hooks/temp-chats/useChattingEffect';
 import ChatsComponent from '@/components/temp-chats/ChatsComponent';
-import JoinChatComponent from '@/components/temp-chats/JoinChatComponent';
-import useUserAccountEffect from '@/hooks/temp-chats/useUserAccountEffect';
-import MenuBarComponent from '@/components/temp-chats/MenuBarComponent';
 import InputComponent from '@/components/temp-chats/InputComponent';
-import DeleteAccountModalComponent from '@/components/temp-chats/DeleteAccountModalComponent';
 import {WithAuth} from '@/components/with-auth';
+import {useSession} from 'next-auth/react';
 
 function TempChats() {
+  const {data: session} = useSession() as any;
   const {messages, sendMessage} = useChattingEffect();
-  const {
-    displayName,
-    setDisplayName,
-    joinChat,
-    logIn,
-    logOut,
-    isLoggedIn,
-    userId,
-    isProfileCreated,
-    deleteAccount
-  } = useUserAccountEffect();
 
-  const messagesAreaComponent = () => {
-    return (
-      <div id="chat-container" className="border rounded-md p-3">
-        <div id="chat-header" className="mb-3">
-          <MenuBarComponent
-            isLoggedIn={isLoggedIn}
-            displayName={displayName}
-            logOut={logOut}
-            logIn={logIn}/>
-        </div>
-        <div>
-          {
-            isLoggedIn
-            && (
-              <>
-                <ChatsComponent messages={messages} currentUserId={userId}/>
-                <InputComponent userId={userId} displayName={displayName}
-                  sendMessage={sendMessage}/>
-              </>
-            )
-          }
-        </div>
-      </div>
-    );
-  };
+  const { user: { id: userId, displayName } } = session;
 
   return (
     <BaseLayout pageTitle="TempChats">
       <div className="grid grid-cols-12 gap-4">
         <div className="col-span-12 md:col-span-12 lg:col-span-8 mb-10">
-          {
-            !isProfileCreated
-              ? <JoinChatComponent
-                displayName={displayName}
-                setDisplayName={setDisplayName}
-                joinChat={joinChat}/>
-              : messagesAreaComponent()
-          }
+          <div id="chat-container" className="border rounded-md p-3">
+            <div id="chat-header" className="mb-3">
+              <span className="ms-2"><strong>{displayName}</strong></span>
+            </div>
+            <div>
+              <ChatsComponent messages={messages} currentUserId={userId}/>
+              <InputComponent userId={userId} displayName={displayName} sendMessage={sendMessage}/>
+            </div>
+          </div>
         </div>
         <div className="col-span-12 md:col-span-12 lg:col-span-4 mb-10">
           <p className="font-bold">Info</p>
           <ul className="my-3 ml-6 list-disc [&>li]:mt-2">
             <li>Messages are automatically deleted
-              after {TEMP_CHATS_MESSAGE_TTL_IN_MILLISECONDS / 60000} minutes.</li>
+              after {TEMP_CHATS_MESSAGE_TTL_IN_MILLISECONDS / 60000} minutes.
+            </li>
             <li>Files are automatically deleted
-              after {TEMP_CHATS_MESSAGE_TTL_IN_MILLISECONDS / 60000} minutes.</li>
+              after {TEMP_CHATS_MESSAGE_TTL_IN_MILLISECONDS / 60000} minutes.
+            </li>
           </ul>
-          {
-            isLoggedIn && <DeleteAccountModalComponent deleteAccount={deleteAccount}/>
-          }
         </div>
       </div>
     </BaseLayout>
