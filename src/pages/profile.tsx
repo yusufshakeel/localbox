99771b4fafdefb54ef8bd5445885f2e70c740f75
Yusuf Shakeel <6522504+yusufshakeel@users.noreply.github.com>
2025-01-09 15:48:24 +0530

    Refactoring

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -12,6 +12,8 @@ import {useSession} from 'next-auth/react';
 import {Button} from '@/components/ui/button';
 import {WithAuth} from '@/components/with-auth';
 import {Pages} from '@/configs/pages';
+import {hasPermissions} from '@/utils/permissions';
+import {PermissionsType} from '@/types/permissions';
 
 function ProfilePage() {
   const {data: session} = useSession() as any;
@@ -23,58 +25,72 @@ function ProfilePage() {
       <div className="grid grid-cols-12 gap-4">
         <div className="col-span-12 lg:col-span-8 mb-10">
           <div className="mb-5">
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button variant="default">Edit</Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent align="start">
-                <DropdownMenuLabel>Username: {user.username}</DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem>Update Details
-                </DropdownMenuItem>
-                <DropdownMenuItem>Update Password
-                </DropdownMenuItem>
-                <DropdownMenuSeparator />
-              </DropdownMenuContent>
-            </DropdownMenu>
+            {
+              hasPermissions(
+                session?.user?.permissions,
+                [`${Pages.profile.id}:${PermissionsType.AUTHORIZED_USE}`]
+              ) && (
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <Button variant="default">Edit</Button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent align="start">
+                    <DropdownMenuLabel>Username: {user.username}</DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem>Update Details
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>Update Password
+                    </DropdownMenuItem>
+                    <DropdownMenuSeparator />
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              )
+            }
           </div>
 
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead className="font-bold">Key</TableHead>
-                <TableHead className="font-bold">Value</TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              <TableRow>
-                <TableCell>Username</TableCell>
-                <TableCell>{user.username}</TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell>Display Name</TableCell>
-                <TableCell>{user.displayName}</TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell>Account Status</TableCell>
-                <TableCell>{user.status}</TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell>Account Type</TableCell>
-                <TableCell>{user.type}</TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell>Permissions</TableCell>
-                <TableCell>
-                  {
-                    user.permissions.sort().map((permission: string) => {
-                      return <p key={permission}><code>{permission}</code></p>;
-                    })
-                  }
-                </TableCell>
-              </TableRow>
-            </TableBody>
-          </Table>
+          {
+            hasPermissions(
+              session?.user?.permissions,
+              [`${Pages.profile.id}:${PermissionsType.AUTHORIZED_VIEW}`]
+            ) && (
+              <Table>
+                <TableHeader>
+                  <TableRow>
+                    <TableHead className="font-bold">Key</TableHead>
+                    <TableHead className="font-bold">Value</TableHead>
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  <TableRow>
+                    <TableCell>Username</TableCell>
+                    <TableCell>{user.username}</TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell>Display Name</TableCell>
+                    <TableCell>{user.displayName}</TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell>Account Status</TableCell>
+                    <TableCell>{user.status}</TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell>Account Type</TableCell>
+                    <TableCell>{user.type}</TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell>Permissions</TableCell>
+                    <TableCell>
+                      {
+                        user.permissions.sort().map((permission: string) => {
+                          return <p key={permission}><code>{permission}</code></p>;
+                        })
+                      }
+                    </TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            )
+          }
         </div>
       </div>
     </BaseLayout>
