1ab3838f782e7548d5068678dc88dbca4edff965
Yusuf Shakeel <6522504+yusufshakeel@users.noreply.github.com>
2025-01-10 21:20:09 +0530

    Refactoring work

--- src/components/UserCannotDeleteUploadedFile.tsx
+++ src/components/UserCannotDeleteUploadedFile.tsx
@@ -2,7 +2,7 @@ export default function UserCannotDeleteUploadedFile() {
   return (
     <div className="my-5 text-sm text-muted-foreground">
       <p>Any files uploaded here are visible to other users.</p>
-      <p>Contact Admin to remove a file.</p>
+      <p>Contact the Admin to remove a file.</p>
     </div>
   );
 }
