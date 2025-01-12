ae85331557c5fa107bb8960a9f3041662b210173
Yusuf Shakeel <6522504+yusufshakeel@users.noreply.github.com>
2025-01-12 15:15:18 +0530

    Download file name

--- src/utils/download.ts
+++ src/utils/download.ts
@@ -1,6 +1,8 @@
+import {getFilename} from '@/utils/filename';
+
 export function handleDownload(directory: string, filename: string) {
   const link = document.createElement('a');
   link.href = `/${directory}/${filename}`;
-  link.download = filename;
+  link.download = getFilename(filename);
   link.click();
 }
